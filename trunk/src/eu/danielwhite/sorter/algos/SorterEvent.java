a1e8d21684b38f60d34d32d6456e4a90f9d01d19
daniel.white <daniel.white@91c5a33c-09e3-11df-a843-f99e4352f152>
2010-01-28 18:40:27 +0000

    Documentation

--- trunk/src/eu/danielwhite/sorter/algos/SorterEvent.java
+++ trunk/src/eu/danielwhite/sorter/algos/SorterEvent.java
@@ -1,11 +1,22 @@
 package eu.danielwhite.sorter.algos;
 
+/**
+ * Event object handed to a SorterListener implementation to let the know the source
+ * of the SorterEvent.
+ * @author dan
+ *
+ * @param <T> The type of data that the sorter is sorting.
+ */
 public class SorterEvent<T extends Comparable<T>> {
 	private final Sorter<T> source;
 	public SorterEvent(Sorter<T> source) {
 		this.source = source;
 	}
 	
+	/**
+	 * The source of the event.
+	 * @return Reference to the sorter object.
+	 */
 	public Sorter<T> getSource() {
 		return source;
 	}
