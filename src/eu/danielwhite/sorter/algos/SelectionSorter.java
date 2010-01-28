acdd48a07bcae073021b13d6c6eefe3313e3102f
daniel.white <daniel.white@91c5a33c-09e3-11df-a843-f99e4352f152>
2010-01-28 18:39:18 +0000

    Documentation

--- src/eu/danielwhite/sorter/algos/SelectionSorter.java
+++ src/eu/danielwhite/sorter/algos/SelectionSorter.java
@@ -2,6 +2,12 @@ package eu.danielwhite.sorter.algos;
 
 import eu.danielwhite.sorter.R;
 
+/**
+ * Implementation of selection sort.
+ * @author dan
+ *
+ * @param <T> The type of data to be sorted.
+ */
 public class SelectionSorter<T extends Comparable<T>> extends Sorter<T> {
 
 	public SelectionSorter(T[] data) {
@@ -15,6 +21,9 @@ public class SelectionSorter<T extends Comparable<T>> extends Sorter<T> {
 
 	@Override
 	public void run() {
+		// this is basically an optimisation of bubble sort.
+		// repeatedly scan the array, finding the minimum value from the unsorted portion of the data.
+		// Once done, swap it with whatever is already there. Repeat until the array is sorted.
 		int length = getDataLength();
 		for(int i = 0; i < length-1; i++) {
 			int min = i;
