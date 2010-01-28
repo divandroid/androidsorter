a34db62345d6986c9df08f9dd77e810de269634f
daniel.white <daniel.white@91c5a33c-09e3-11df-a843-f99e4352f152>
2010-01-28 18:23:57 +0000

    Documentation

--- trunk/src/eu/danielwhite/sorter/algos/BubbleSorter.java
+++ trunk/src/eu/danielwhite/sorter/algos/BubbleSorter.java
@@ -2,6 +2,15 @@ package eu.danielwhite.sorter.algos;
 
 import eu.danielwhite.sorter.R;
 
+/**
+ * Bubble sort implementation.
+ * 
+ * http://en.wikipedia.org/wiki/Bubble_sort
+ * 
+ * @author dan
+ *
+ * @param <T> The type of the array to be sorted.
+ */
 public class BubbleSorter<T extends Comparable<T>> extends Sorter<T> {
 
 	public BubbleSorter(T[] data) {
@@ -13,10 +22,15 @@ public class BubbleSorter<T extends Comparable<T>> extends Sorter<T> {
 		return R.string.bubble_sort;
 	}
 
+	/**
+	 * The thread that is executed.
+	 */
 	@Override
 	public void run() {
 		int iLimit = getDataLength();
+		// Go through the array, swapping each element with the one next to it if they are out of order.
     	for(int i = iLimit-1; i >= 0; i--) {
+    		// Keep going until no items have been swapped on a single pass through the array.
     		boolean swapped = false;
     		for(int j = 0; j < i; j++) {
 
@@ -28,6 +42,7 @@ public class BubbleSorter<T extends Comparable<T>> extends Sorter<T> {
     		// if no swaps occurred, then stop as it's finished.
     		if(!swapped) break;
     	}
+    	// let everybody know we've finished.
     	fireSorterFinished();
 		
 	}
